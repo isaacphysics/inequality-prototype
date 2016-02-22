92b9a3e7d0711bcf2d19b3ff5e780e2f822a8677
Andrea Franceschini <andrea.franceschini@gmail.com>
2016-02-22 17:42:53 +0000

    Symbols can be configured with letters.
    - Added an OpenType font
    - Modified symbols' drawing code to draw letters
    - Bounding box is computed from the actual glyphs

--- src/app/symbol.ts
+++ src/app/symbol.ts
@@ -2,8 +2,17 @@ import { Widget, Rect } from './widget.ts'
 
 export
 class Symbol extends Widget {
-	constructor(p: any, private s: any) {
+	letter = "NULL";
+	bounds: Rect = null;
+
+	constructor(p: any, private s: any, letter: string) {
 		super(p, s);
+		this.letter = letter;
+
+		// WHY THE OFFSET?!
+		var offset = 100;
+		var box = s.font.textBounds(letter, offset, offset, this.scale * 120);
+		this.bounds = new Rect( box.x-(offset + box.w/2), box.y-(offset - box.h/2), box.w, box.h);
 
 		this.dockingPoints = _.map([0, 1, 5], n => {
 			// Mind the minus sign.
@@ -16,15 +25,19 @@ class Symbol extends Widget {
 		this.children = [null, null, null];
 	}
 
+	boundingBox(): Rect {
+		// These numbers are hardcoded, but I suppose that's OK for now...
+		return new Rect(this.position.x + this.bounds.x, this.position.y + this.bounds.y, this.bounds.w, this.bounds.h);
+	}
+
 	draw() {
 		super.draw();
 
 		this.p.fill(0).strokeWeight(0);
-		this.p.textFont("Georgia")
-			.textStyle(this.p.ITALIC)
+		this.p.textFont(this.s.font)
 			.textSize(120 * this.scale)
 			.textAlign(this.p.CENTER, this.p.CENTER)
-			.text("e", this.position.x, this.position.y);
+			.text(this.letter, this.position.x, this.position.y);
 		this.p.strokeWeight(1);
 	}
 }
